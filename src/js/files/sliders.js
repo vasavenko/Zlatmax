db0e6369bad908e351ef7f4ee488514ce8871ba8
vasavenko <78298511+vasavenko@users.noreply.github.com>
2022-08-29 15:17:36 +0300

    main page

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -176,8 +176,7 @@ function initSliders() {
       breakpoints: {
         320: {
           slidesPerView: 1,
-          spaceBetween: 0,
-          autoHeight: true,
+          spaceBetween: 10,
         },
         768: {
           slidesPerView: 2,
@@ -189,6 +188,93 @@ function initSliders() {
         },
         1370: {
           slidesPerView: 4,
+          spaceBetween: 30,
+        },
+      },
+      // События
+      on: {
+        // init: function (swiper) {
+        //   const allSlides = document.querySelectorAll('.product-card');
+        //   if (allSlides.length > 4) {
+        //     swiper.params.loop = true;
+        //   }
+        //   console.log(swiper);
+        //   console.log(swiper.params.loop);
+        // },
+      },
+    });
+  }
+  //----------------------------------------------------------
+  if (document.querySelector('.product-new')) {
+    // Указываем скласс нужного слайдера
+    // Создаем слайдер
+    new Swiper('.product-new__slider', {
+      // Указываем скласс нужного слайдера
+      // Подключаем модули слайдера
+      // для конкретного случая
+      modules: [Navigation, Pagination, Autoplay],
+      observer: true,
+      observeParents: true,
+      slidesPerView: 3,
+      spaceBetween: 30,
+      parallax: true,
+      // autoHeight: true,
+      speed: 800,
+
+      //touchRatio: 0,
+      //simulateTouch: false,
+
+      // loop: true,
+
+      //preloadImages: false,
+      //lazy: true,
+
+      //Эффекты
+      //effect: 'fade',
+      autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      },
+
+      // Пагинация
+      //
+      pagination: {
+        el: '.product-new__dotts',
+        clickable: true,
+        dynamicBullets: true,
+      },
+      //
+
+      // Скроллбар
+      /*
+			scrollbar: {
+				el: '.swiper-scrollbar',
+				draggable: true,
+			},
+			*/
+
+      // Кнопки "влево/вправо"
+      // navigation: {
+      //   prevEl: '.swiper-button-prev',
+      //   nextEl: '.swiper-button-next',
+      // },
+
+      // Брейкпоинты
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 10,
+        },
+        768: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+        992: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        1360: {
+          slidesPerView: 3,
           spaceBetween: 30,
         },
       },
